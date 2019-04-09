95a8807cd84899cfd71777b9cbbc8ceb23d6c5a2
Matheus Alves Albuquerque <math.albuquerque@outlook.com>
2019-04-09 15:32:18 -0300

    autenticação no watson assistan V2

---
+++ autentication.js
@@ -0,0 +1,15 @@
+;(()=>{
+    const AssistantV2 = require('watson-developer-cloud/assistant/v2');
+    const {credentials} = require('./credentials');
+
+    const assistant = new AssistantV2({
+    version: credentials.Sust_Homol.version,
+    iam_apikey: credentials.Sust_Homol.api_key,
+    url: credentials.Sust_Homol.url
+    });
+
+
+
+    if(assistant) console.log(`Logado com Sucesso `);
+    module.exports=assistant;
+})()
