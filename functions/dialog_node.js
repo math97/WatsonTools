ea111455e08575a237f1e266998d6a07751c777a
Victor Nocera Domingues Correa <noceravictor@gmail.com>
2019-04-10 13:56:04 -0300

    alteraçao no credentials e listDialog_node

--- functions/dialog_node.js
+++ functions/dialog_node.js
@@ -1,18 +1,24 @@
-const autentication = require('./autentication')
-const { credentials } = require('./credentials')
+const authentication = require('./authentication')
+const credentials = require('./credentials')
 
-const listDialog = () => {
+function listDialogNodes() {
   const params = {
     workspace_id: credentials.Sust_Homol.workspace_id
   }
-  console.log('Dialog')
 
-  autentication
+  if (authentication.assistant) console.log(`Logado com Sucesso `)
+
+  authentication.assistant
     .listDialogNodes(params)
     .then(res => {
       console.log(JSON.stringify(res, null, 2))
+      if (res.pagination.next_cursor) {
+        params.cursor = res.pagination.next_cursor
+      }
     })
     .catch(err => {
       console.log(err)
     })
 }
+
+listDialogNodes()
